7fd761ae54b4964a4e32000dbeee3f8004688b1a
JLCucumber <jasonli1207@foxmail.com>
2026-04-29 13:38:27 +0100

    add: horizontal stack

--- experiments/01_semantic_sanity_check/run.py
+++ experiments/01_semantic_sanity_check/run.py
@@ -4,6 +4,7 @@ sys.path.insert(0, "/workspace")
 import cv2
 import os
 import torch
+import numpy as np
 
 from viplanner_ros2.m2f_inference import Mask2FormerInference
 
@@ -12,8 +13,6 @@ CONFIG = f"{M2F_DIR}/mask2former_r50_8xb2-lsj-50e_coco-panoptic.py"
 CHECKPOINT = f"{M2F_DIR}/mask2former_r50_8xb2-lsj-50e_coco-panoptic_20230118_125535-54df384a.pth"
 
 m2f = Mask2FormerInference(config_file=CONFIG, checkpoint_file=CHECKPOINT)
-
-import cv2
 
 input_dir = "/workspace/experiments/01_semantic_sanity_check/inputs"
 output_dir = "/workspace/experiments/01_semantic_sanity_check/outputs"
@@ -25,5 +24,13 @@ for fname in os.listdir(input_dir):
     img = cv2.imread(os.path.join(input_dir, fname))
     img = cv2.resize(img, (640,480))
     mask = m2f.predict(img)
-    cv2.imwrite(os.path.join(output_dir, fname), mask)
+
+    if mask.shape != img.shape:
+        mask = cv2.resize(mask, (img.shape[1], img.shape[0]))
+    
+    overlay = cv2.addWeighted(img, 0.5, mask, 0.5, 0)
+
+    combined = np.hstack([img, mask, overlay])
+    cv2.imwrite(os.path.join(output_dir, fname), combined)
+
     print(f"done: {fname}")
